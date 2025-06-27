071691ef31b5f6768f7d6c332b67d27fbc3199ac
Derek Lisboa <derekmlisboa@gmail.com>
2025-06-27 17:17:35 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/devsuperior/hr_worker/entities/Worker.java
+++ hr-worker/src/main/java/com/devsuperior/hr_worker/entities/Worker.java
@@ -3,11 +3,12 @@ package com.devsuperior.hr_worker.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 
 @Entity
